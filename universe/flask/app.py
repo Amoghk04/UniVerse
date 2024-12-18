3ab835557ee0c99874155c5a1af41c628765dd77
Amoghk04 <kalasapuraamogh@gmail.com>
2024-12-18 13:42:01 +0530

    UniVerse: Added Boiler Plate for Flask

---
+++ universe/flask/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def main():
+    # TODO: DO stuff
+    return jsonify({"Result":"200"})
+
+if __name__=="__main__":
+    app.run(debug=True, port=5000)
